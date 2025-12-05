9e80fcbd5b706ebdb3e337ef48667aef01341e0c
Tejash Mishra <mtejash07@gmail.com>
2025-12-06 04:10:56 +0530

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -113,17 +113,5 @@ export interface Database {
         }
       }
     }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
   }
 }
