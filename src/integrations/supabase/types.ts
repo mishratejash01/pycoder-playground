b246873cfb4c5fd56229dcfad6bc47a4aa892dd6
Tejash Mishra <mtejash07@gmail.com>
2025-12-07 03:25:20 +0530

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -103,7 +103,6 @@ export type Database = {
           private_testcases?: Json | null
         }
       }
-      // --- NEW TABLE FOR PROCTORED EXAMS ---
       iitm_exam_question_bank: {
         Row: {
           id: string
